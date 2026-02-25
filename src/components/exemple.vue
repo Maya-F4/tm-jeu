9f0d33ca1aedd2eff584a061f42a248ac50ab84a
Maya-F4 <pr32uyu@eduvaud.ch>
2026-02-25 10:56:34 +0100

    configuration vs code

---
+++ src/components/exemple.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+
+const compteur = ref(0)
+
+const trop = computed(()=>{
+    return compteur.value > 10
+})
+/*
+const cliquer = ()=>{
+    compteur.value = compteur.value + 1
+}
+*/
+
+function cliquer(){
+    compteur.value = compteur.value + 1
+}
+
+</script>
+<template>
+    <div>
+        C'est un exemple avec un bouton
+
+        <button 
+        class="rounded-2xl shadow-lg p-4 text-white font-bold mt-4 bg-rose-300 hover:bg-rose-800 transition-colors duration-300"
+        @click="cliquer">joli bouton</button>
+
+        On a cliqué {{ compteur }}
+
+        <div>
+            <p v-if="trop">C'est trop !</p>
+        </div>
+    </div>
+</template>
